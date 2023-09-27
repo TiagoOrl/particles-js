5cf84998bba52566437e4ea9b9081781d3249d58
Tiago Orlando <tiago_orlando@yahoo.com>
2023-09-27 00:35:18 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+let item1
+let item2
+let size
+let diam
+
+function setup() {
+  size = 600
+  diam = 20
+  createCanvas(size, size);
+  item1 = new Item(250, 40, size, size, diam, random(-10, 10), 0, 1.6, 0.5);
+  item2 = new Item(300, 40, size, size, diam, random(-10, 10), 0, 1.6, 0.5);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  circle(item1.x, item1.y, item1.diameter);
+  circle(item2.x, item2.y, item2.diameter);
+  item1.update();
+  item2.update();
+}
+
+function mouseClicked() {
+  item1.addVel(random(0, 10), random(0, 10))
+  item2.addVel(random(0, 10), random(0, 10))
+}
---
+++ src/item.js
@@ -0,0 +1,68 @@
+
+
+class Item {
+    constructor(x, y, limX, limY, diameter, vx = 0, vy = 0, loss = 0, grav = 0) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.limX = limX;
+        this.limY = limY;
+        this.diameter = diameter;
+        this.loss = loss;
+        this.grav = grav;
+    }
+
+    update() {
+        if (this.grav != 0 && this.y + this.diameter / 2 < this.limY) {
+                this.vy += this.grav
+        }
+                
+        if (this.x + this.diameter / 2 >= this.limX) {
+            this.x = this.limX - this.diameter / 2;
+            this.vx = -this.vx;
+            this.vx += this.loss;
+        }
+
+        if (this.x - this.diameter / 2 <= 0) {
+            this.x = this.diameter / 2;
+            this.vx = -this.vx;
+            this.vx -= this.loss;
+        }
+            
+
+        if (this.y + this.diameter / 2 >= this.limY) {
+            this.y = this.limY - this.diameter / 2;
+            this.vy = -this.vy;
+            this.vy += this.loss;
+        }
+
+        if (this.y - this.diameter / 2 <= 0) {
+            this.y = this.diameter / 2;
+            this.vy = -this.vy;
+            this.vy -= this.loss;
+        }
+            
+
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    addVel(vx, vy) {
+        if (this.vx < 0)
+            this.vx -= vx;
+        else
+            this.vx += vx;
+
+        if (this.vy < 0)
+            this.vy -= vy;
+        else
+            this.vy += vy;
+    }
+
+
+}
+
+if (typeof module !== "undefined") {
+    module.exports = { Item };
+}
